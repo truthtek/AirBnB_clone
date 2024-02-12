4aa51382c2a56c18bc7f1aa7ea31a20b545fffc0
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-02-12 09:45:33 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ Module containing the BaseModel class
 
 import uuid
 from datetime import datetime
+from models import storage
 
 class BaseModel:
     """
@@ -26,6 +27,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
@@ -39,6 +41,7 @@ class BaseModel:
         with the current datetime
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
@@ -50,24 +53,3 @@ class BaseModel:
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
-
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
-
-    print("--")
-    my_new_model = BaseModel(**my_model_json)
-    print(my_new_model)
-    print(my_new_model.id)
-    print(type(my_new_model.created_at))
-
-
