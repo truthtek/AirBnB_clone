b990086630d77fe9fd5a8ceed21fa9da911a6f74
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-02-13 10:40:16 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,13 @@
+#!/usr/bin/python3
+"""Defines the Amenity class."""
 from models.base_model import BaseModel
 
+
 class Amenity(BaseModel):
-    """Amenity class that inherits from BaseModel."""
-    
-    def __init__(self, *args, **kwargs):
-        """Initialize Amenity class."""
-        super().__init__(*args, **kwargs)
-        self.name = ""
+    """Represent an amenity.
+
+    Attributes:
+        name (str): The name of the amenity.
+    """
+
+    name = ""
