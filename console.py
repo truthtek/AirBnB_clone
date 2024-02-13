88a01ed506b9186180640b5260a023246c628874
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-02-13 21:23:09 +0100

    Update console.py

--- console.py
+++ console.py
@@ -1,51 +1,17 @@
-from cmd import Cmd
+import cmd
 import re
 from shlex import split
-from models import storage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
 
 
-def parse(arg):
-    curly_braces = re.search(r"\{(.*?)\}", arg)
-    brackets = re.search(r"\[(.*?)\]", arg)
-    if curly_braces is None:
-        if brackets is None:
-            return [i.strip(",") for i in split(arg)]
-        else:
-            lexer = split(arg[:brackets.span()[0]])
-            retl = [i.strip(",") for i in lexer]
-            retl.append(brackets.group())
-            return retl
-    else:
-        lexer = split(arg[:curly_braces.span()[0]])
-        retl = [i.strip(",") for i in lexer]
-        retl.append(curly_braces.group())
-        return retl
-
-
-class HBNBCommand(Cmd):
-    prompt = "(hbnb) "
-    __classes = {
-        "BaseModel",
-        "User",
-        "State",
-        "City",
-        "Place",
-        "Amenity",
-        "Review"
-    }
+class MyPythonAssistant(cmd.Cmd):
+    intro = "Welcome to MyPythonAssistant! Type help or ? to list commands.\n"
+    prompt = "(python) "
 
     def emptyline(self):
         pass
 
     def default(self, arg):
-        argdict = {
+        arg_dict = {
             "all": self.do_all,
             "show": self.do_show,
             "destroy": self.do_destroy,
@@ -54,78 +20,77 @@ class HBNBCommand(Cmd):
         }
         match = re.search(r"\.", arg)
         if match is not None:
-            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
-            match = re.search(r"\((.*?)\)", argl[1])
+            arg_list = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = re.search(r"\((.*?)\)", arg_list[1])
             if match is not None:
-                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
-                if command[0] in argdict.keys():
-                    call = "{} {}".format(argl[0], command[1])
-                    return argdict[command[0]](call)
+                command = [arg_list[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in arg_dict.keys():
+                    call = "{} {}".format(arg_list[0], command[1])
+                    return arg_dict[command[0]](call)
         print("*** Unknown syntax: {}".format(arg))
         return False
 
     def do_quit(self, arg):
+        """Exit MyPythonAssistant"""
         return True
 
     def do_EOF(self, arg):
+        """Exit MyPythonAssistant"""
         print("")
         return True
 
     def do_create(self, arg):
-        argl = parse(arg)
-        if len(argl) == 0:
+        """Create a new object"""
+        arg_list = split(arg)
+        if len(arg_list) == 0:
             print("** class name missing **")
-        elif argl[0] not in HBNBCommand.__classes:
-            print("** class doesn't exist **")
         else:
-            print(eval(argl[0])().id)
-            storage.save()
+            print("Creating a new {} object...".format(arg_list[0]))
+            # Perform object creation logic here
 
     def do_show(self, arg):
-        argl = parse(arg)
-        objdict = storage.all()
-        if len(argl) == 0:
+        """Show details of an object"""
+        arg_list = split(arg)
+        if len(arg_list) == 0:
             print("** class name missing **")
-        elif argl[0] not in HBNBCommand.__classes:
-            print("** class doesn't exist **")
-        elif len(argl) == 1:
+        elif len(arg_list) == 1:
             print("** instance id missing **")
-        elif "{}.{}".format(argl[0], argl[1]) not in objdict:
-            print("** no instance found **")
         else:
-            print(objdict["{}.{}".format(argl[0], argl[1])])
+            print("Showing details of {} with id: {}".format(arg_list[0], arg_list[1]))
+            # Perform object show logic here
 
     def do_destroy(self, arg):
-        argl = parse(arg)
-        objdict = storage.all()
-        if len(argl) == 0:
+        """Destroy an object"""
+        arg_list = split(arg)
+        if len(arg_list) == 0:
             print("** class name missing **")
-        elif argl[0] not in HBNBCommand.__classes:
-            print("** class doesn't exist **")
-        elif len(argl) == 1:
+        elif len(arg_list) == 1:
             print("** instance id missing **")
-        elif "{}.{}".format(argl[0], argl[1]) not in objdict.keys():
-            print("** no instance found **")
         else:
-            del objdict["{}.{}".format(argl[0], argl[1])]
-            storage.save()
+            print("Destroying {} with id: {}".format(arg_list[0], arg_list[1]))
+            # Perform object destroy logic here
 
     def do_all(self, arg):
-        argl = parse(arg)
-        if len(argl) > 0 and argl[0] not in HBNBCommand.__classes:
-            print("** class doesn't exist **")
+        """List all objects"""
+        arg_list = split(arg)
+        if len(arg_list) > 0:
+            print("Listing all {} objects...".format(arg_list[0]))
+            # Perform filtered object list logic here
         else:
-            objl = []
-            for obj in storage.all().values():
-                if len(argl) > 0 and argl[0] == obj.__class__.__name__:
-                    objl.append(obj.__str__())
-                elif len(argl) == 0:
-                    objl.append(obj.__str__())
-            print(objl)
+            print("Listing all objects...")
+            # Perform complete object list logic here
 
     def do_count(self, arg):
-        argl = parse(arg)
+        """Count the number of objects"""
+        arg_list = split(arg)
         count = 0
-        for obj in storage.all().values():
-            if argl[0] == obj.__class__.__name__:
-                count += 1
+        if len(arg_list) > 0:
+            print("Counting {} objects...".format(arg_list[0]))
+            # Perform filtered object count logic here
+        else:
+            print("Counting all objects...")
+            # Perform complete object count logic here
+
+
+if __name__ == '__main__':
+    MyPythonAssistant().cmdloop()
