94053c2ff36218923c6aca80e55111400f62633f
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-02-13 10:41:01 +0100

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,19 +1,33 @@
+#!/usr/bin/python3
+"""Defines the Place class."""
 from models.base_model import BaseModel
 
+
 class Place(BaseModel):
-    """Place class that inherits from BaseModel."""
-    
-    def __init__(self, *args, **kwargs):
-        """Initialize Place class."""
-        super().__init__(*args, **kwargs)
-        self.city_id = ""
-        self.user_id = ""
-        self.name = ""
-        self.description = ""
-        self.number_rooms = 0
-        self.number_bathrooms = 0
-        self.max_guest = 0
-        self.price_by_night = 0
-        self.latitude = 0.0
-        self.longitude = 0.0
-        self.amenity_ids = []
+    """Represent a place.
+
+    Attributes:
+        city_id (str): The City id.
+        user_id (str): The User id.
+        name (str): The name of the place.
+        description (str): The description of the place.
+        number_rooms (int): The number of rooms of the place.
+        number_bathrooms (int): The number of bathrooms of the place.
+        max_guest (int): The maximum number of guests of the place.
+        price_by_night (int): The price by night of the place.
+        latitude (float): The latitude of the place.
+        longitude (float): The longitude of the place.
+        amenity_ids (list): A list of Amenity ids.
+    """
+
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
