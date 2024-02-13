1b8f8d4e130c6b067be8c472bae96878ade50bb9
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-02-13 10:05:53 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,49 @@
 #!/usr/bin/python3
 """
-Module containing the instantiation of FileStorage
+Module for BaseModel class
 """
+import uuid
+from datetime import datetime
 
-from models.engine.file_storage import FileStorage
 
-storage = FileStorage()
-storage.reload()
+class BaseModel:
+    """
+    BaseModel class that defines common attributes/methods for other classes
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes a new instance of BaseModel
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        Returns a string representation of BaseModel instance
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates the public instance attribute updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values of __dict__ of the instance
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
