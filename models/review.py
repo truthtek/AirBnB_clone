2f89e4c8b70f3eb619cd1f64f4156f5b44f67afd
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-02-13 10:41:20 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,11 +1,17 @@
+#!/usr/bin/python3
+"""Defines the Review class."""
 from models.base_model import BaseModel
 
+
 class Review(BaseModel):
-    """Review class that inherits from BaseModel."""
-    
-    def __init__(self, *args, **kwargs):
-        """Initialize Review class."""
-        super().__init__(*args, **kwargs)
-        self.place_id = ""
-        self.user_id = ""
-        self.text = ""
+    """Represent a review.
+
+    Attributes:
+        place_id (str): The Place id.
+        user_id (str): The User id.
+        text (str): The text of the review.
+    """
+
+    place_id = ""
+    user_id = ""
+    text = ""
