1c90756e1e9040349ce2defbecb5640e62c58dba
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-02-13 10:04:04 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,52 +1,38 @@
 #!/usr/bin/python3
 """
-Module containing the BaseModel class
+Module for BaseModel class
 """
-
 import uuid
 from datetime import datetime
-from models import storage
+
 
 class BaseModel:
     """
-    BaseModel class that defines all common attributes/methods
-    for other classes
+    BaseModel class that defines common attributes/methods for other classes
     """
-
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """
-        Constructor method to initialize BaseModel instance
+        Initializes a new instance of BaseModel
         """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-                elif key != "__class__":
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """
-        String representation of BaseModel instance
+        Returns a string representation of BaseModel instance
         """
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """
-        Updates the public instance attribute updated_at
-        with the current datetime
+        Updates the public instance attribute updated_at with the current datetime
         """
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
         """
-        Returns a dictionary containing all keys/values of __dict__
-        of the instance
+        Returns a dictionary containing all keys/values of __dict__ of the instance
         """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
