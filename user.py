4e37136a4de0ced05471d4c8b1c4de9b1c8560b8
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-02-13 10:25:10 +0100

    Update user.py

--- user.py
+++ user.py
@@ -3,10 +3,11 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """User class that inherits from BaseModel."""
     
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
     def __init__(self, *args, **kwargs):
-        """Initialize User class."""
+        """Initialize User instance."""
         super().__init__(*args, **kwargs)
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
