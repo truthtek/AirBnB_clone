171c124de0548778ca5d3cf4626c82d7356c613d
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-02-13 10:40:41 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,10 +1,16 @@
+#!/usr/bin/python3
+"""Defines the City class."""
 from models.base_model import BaseModel
 
+
 class City(BaseModel):
-    """City class that inherits from BaseModel."""
-    
-    def __init__(self, *args, **kwargs):
-        """Initialize City class."""
-        super().__init__(*args, **kwargs)
-        self.state_id = ""
-        self.name = ""
+    """Represent a city.
+
+    Attributes:
+        state_id (str): The state id.
+        name (str): The name of the city.
+    """
+
+    state_id = ""
+    name = ""
+
