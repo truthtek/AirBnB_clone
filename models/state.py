c10c1d0e24e17d6252465f363836e1cd4045abbe
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-02-13 10:41:44 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,9 +1,13 @@
+#!/usr/bin/python3
+"""Defines the State class."""
 from models.base_model import BaseModel
 
+
 class State(BaseModel):
-    """State class that inherits from BaseModel."""
-    
-    def __init__(self, *args, **kwargs):
-        """Initialize State class."""
-        super().__init__(*args, **kwargs)
-        self.name = ""
+    """Represent a state.
+
+    Attributes:
+        name (str): The name of the state.
+    """
+
+    name = ""
